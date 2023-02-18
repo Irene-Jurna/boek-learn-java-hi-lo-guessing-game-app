11d72d1caa6160a7d169fafe17c41943b71e4272
Irene Jurna <irene.jurna@gmail.com>
2023-02-18 17:35:35 +0100

    Challenges Expaning range & counting tries

--- src/HiLo.java
+++ src/HiLo.java
@@ -4,23 +4,23 @@ public class HiLo {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String playAgain = "";
+        int numberOfTries = 0;
 
         do {
-            // Create a random number for the user to guess
-            int theNumber = (int)(Math.random() * 100 + 1);
-            // System.out.println(theNumber);
+            int theNumber = (int) (Math.random() * 200 + 1 - 100);
 
             int guess = 0;
             while (guess != theNumber) {
-                System.out.println("Guess a number between 1 and 100:");
+                numberOfTries ++;
+                System.out.println("Guess a number between -100 and 100:");
                 guess = scan.nextInt();
                 if (guess < theNumber)
-                    System.out.println(guess + " is too low. Try again.");
+                    System.out.println(guess + " is too low. Try again.\nNumber of tries: " + numberOfTries);
                 else if (guess > theNumber)
-                    System.out.println(guess + " is too high. Try again.");
+                    System.out.println(guess + " is too high. Try again.\nNumber of tries: " + numberOfTries);
                 else
-                    System.out.println(guess + " is correct. You win!");
-            } // End of while loop for guessing
+                    System.out.println(guess + " is correct. You win!\nYou guessed the right number in " + numberOfTries + " times.");
+            }
             System.out.println("Would you like to play again (y/n)?");
             playAgain = scan.next();
         } while (playAgain.equalsIgnoreCase("y"));
